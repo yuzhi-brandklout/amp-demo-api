290e5757fc515516435f1981238f80974aa679d2
shushu <shushu@hanfu.la>
2024-10-09 14:27:28 +0800

    json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.all('/json', (req, res, next) => {
+  res.json({ now: new Date().toString() });
 });
 
 module.exports = router;
