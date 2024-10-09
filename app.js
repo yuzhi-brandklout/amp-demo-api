125f2abcfce09f37a903ca2908e19082d2f714f6
shushu <shushu@hanfu.la>
2024-10-09 15:08:06 +0800

    refactor

--- app.js
+++ app.js
@@ -1,41 +1,18 @@
-const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-
-const indexRouter = require('./routes/index');
-
 const app = express();
 
-app.use(cors({
-  origin: (origin, callback) => callback(null, origin),
-  credentials: true,
-}));
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+const cors = require('cors')({
+  origin: (origin, callback) => callback(null, origin),
+  credentials: true,
 });
+app.use(cors);
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+const json = (req, res) => {
+  res.json({ now: new Date().toString() });
+};
+app.use('/json', json);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(process.env.PORT || 3000);
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.all('/json', (req, res, next) => {
-  res.json({ now: new Date().toString() });
-});
-
-module.exports = router;
