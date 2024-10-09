26caaaf5adb7196c0f7d371f4adfaf38f06bcb6f
shushu <shushu@hanfu.la>
2024-10-09 14:37:29 +0800

    cors

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
+
+app.use(cors({
+  origin: (origin, callback) => callback(null, origin),
+  credentials: true,
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,12 +23,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
